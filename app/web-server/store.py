2b2fdaa3316c13bbf895c915b7a6f604b6e3a493
Kenyi Hancco <kenyihq@gmail.com>
2022-11-03 12:54:09 -0500

    Added dependecies in requirements

--- app/web-server/store.py
+++ app/web-server/store.py
@@ -7,3 +7,10 @@ def get_categories():
     res = requests.get(URL)
     print(res.status_code)
     print(res.text)
+
+    categories = res.json()
+
+    for category in categories:
+        name = category["name"]
+        img = category["image"]
+        print(f"{name} : {img}")
