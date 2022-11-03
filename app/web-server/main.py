6c82593691682493c8b2f6c1db788d90cf0e961f
Kenyi Hancco <kenyihq@gmail.com>
2022-11-03 13:05:59 -0500

    Added fastapi library

--- app/web-server/main.py
+++ app/web-server/main.py
@@ -1,5 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return '''
+        <h1>Soy un H1</h1>
+        <h2>Soy un H2</h2>
+        <h3>Soy un H3</h3>
+        <h4>Soy un H4</h4>
+    '''
 
 def run():
     store.get_categories()
